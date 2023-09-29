81fbe9ef095523c2037e9ecd2b525a0f087426a4
Skyfe24 <fabio.erase@gmail.com>
2023-09-29 17:41:08 +0200

    Caricamento con spinner

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@
             <ul class="me-auto mb-2 mb-lg-0 nav1 d-flex align-items-center ">
                 <img src="../assets/assets/BOOLlogo.svg" alt="Logo BoolBnB" class="logo">
                 <li>
-                    <RouterLink class="nav-link active" :to="{ name: 'estates' }">Annunci</RouterLink>
+                    <RouterLink class="nav-link active a1" :to="{ name: 'estates' }">Annunci</RouterLink>
                 </li>
             </ul>
         </nav>
@@ -21,6 +21,11 @@ header {
     background: rgb(2, 0, 36);
     background: linear-gradient(90deg, rgba(2, 0, 36, 0.9920343137254902) 0%, rgba(9, 79, 121, 1) 32%, rgba(105, 223, 247, 1) 89%);
     height: 80px;
+    font-weight: bold;
+}
+
+.a1 {
+    margin-left: 25px;
 }
 
 .nav1 {
