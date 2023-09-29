cc0f6e6ff621251626b1980889002e1638b38178
marcocareddu <saldumaccu@tuta.io>
2023-09-29 11:36:05 +0200

    Added Card $ Loader

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,3 +1,29 @@
-<script></script>
-<template></template>
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+    props: { data: Object, isDetail: Boolean },
+    components: { RouterLink }
+}
+</script>
+
+<template>
+    <div class="card col-12 m-3 text-bg-dark">
+        <img :src="data.thumb" class="card-img-top" alt="...">
+        <div class="card-body d-flex flex-column justify-content-between">
+            <div>
+                <h5 class="card-title">{{ data.name }}</h5>
+                <p class="card-text">{{ data.description }}</p>
+            </div>
+
+            <!-- Show Button -->
+            <RouterLink v-if="!isDetail" :to="{ name: 'estate-detail', params: { id: data.id } }"
+                class="btn btn-outline-primary mx-3">
+                <i class="fa-solid fa-eye"></i>
+            </RouterLink>
+
+        </div>
+    </div>
+</template>
+
 <style></style>
