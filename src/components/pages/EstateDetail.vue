5179f5b5c1f9175dc682aab8d30b423ac0ab8a89
marcocareddu <saldumaccu@tuta.io>
2023-09-29 15:50:42 +0200

    Added detail page

--- src/components/pages/EstateDetail.vue
+++ src/components/pages/EstateDetail.vue
@@ -1,21 +1,43 @@
 <script>
+import axios from 'axios';
+import AppCard from '@/components/AppCard.vue';
+import AppLoader from '@/components/AppLoader.vue';
+
+
+// Api Endpoints
+const baseEndpoint = 'http://127.0.0.1:8000/api/estates/'
 
 export default {
-  name: 'EstateDetail',
+  name: 'estateDetail',
+  components: { AppCard, AppLoader },
   data() {
-    return {}
+    return {
+      estate: [],
+      apiLoading: false
+    }
   },
-  components: {},
-  props: {},
-  computed: {},
-  methods: {}
-}
+  methods: {
+    getEstate(baseEndpoint = 'http://127.0.0.1:8000/api/estates/') {
+      this.apiLoading = true;
+      axios.get(baseEndpoint + `${this.$route.params.id}`)
+        .then(res => {
+          this.estate = res.data;
+        })
+        .catch(err => { console.log(err) })
+        .then(() => { this.apiLoading = false })
+    }
+  },
+  created() {
+    this.getEstate();
+  }
+};
 </script>
 
 <template>
-EstateDetail Page
+  <div class="container">
+    <AppLoader v-if="apiLoading" />
+    <AppCard v-else :data="estate" :isDetail="true" />
+  </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
