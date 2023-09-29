6dc79e316a8d4ec8eab477ec6d54c37157c73110
cloudhuh <claudiu.huhulia@gmail.com>
2023-09-29 18:06:39 +0200

    fixed searchform button

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -14,7 +14,7 @@ export default {
         <div class="input-group my-3">
             <input type="text" class="form-control" placeholder="Cerca annuncio" aria-describedby="button-addon2"
                 v-model.trim="searchTerm" @keyup="$emit('term-change', searchTerm)">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Cerca</button>
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Cerca</button>
         </div>
     </form>
 </template>
