282bd35934dfd73ee615e9a5e0e41051a088985f
Skyfe24 <fabio.erase@gmail.com>
2023-10-02 15:53:49 +0200

    AppFooter text completed

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,4 +1,33 @@
 <script></script>
 <template>
-    gg
+    <div class="container">
+        <footer>
+            <div>BoolBnB 2023 creato da: <span class="tribute">Claudiu -</span> <span class="tribute">Fabio -</span>
+                <span class="tribute"> Marco -</span><span class="tribute"> Nicola -</span><span class="tribute">
+                    Tommaso</span>
+            </div>
+
+        </footer>
+    </div>
 </template>
+
+<Style scoped lang="scss">
+footer {
+    position: absolute;
+    bottom: 0px;
+    margin-left: 14px;
+}
+
+.h7 {
+    font-weight: 700;
+}
+
+.tribute {
+
+    font-weight: 700;
+}
+
+.tribute:hover {
+    color: #FFBD59;
+}
+</Style>
