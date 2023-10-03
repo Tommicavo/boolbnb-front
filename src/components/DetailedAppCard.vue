62be5adb6faefe3b8ddcbb22607cbd992deffdbc
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-10-03 12:45:21 +0200

    responsive map

--- src/components/DetailedAppCard.vue
+++ src/components/DetailedAppCard.vue
@@ -3,51 +3,54 @@
 import BasicMap from '@/components/BasicMap.vue';
 
 export default {
-  name: 'DetailedAppCard',
-  data() {
-    return {}
-  },
-  components: {
-    BasicMap
-  },
-  props: {
-    estate: Object
-  },
-  computed: {
+    name: 'DetailedAppCard',
+    data() {
+        return {}
+    },
+    components: {
+        BasicMap
+    },
+    props: {
+        estate: Object
+    },
+    computed: {
 
     },
-  methods: {
-    getImagePath(image){
-        const url = image.url;
-        return `http://127.0.0.1:8000/storage/${url}`;
+    methods: {
+        getImagePath(image) {
+            const url = image.url;
+            return `http://127.0.0.1:8000/storage/${url}`;
+        }
+    },
+    created() {
+        console.log();
     }
-  },
-  created(){
-    console.log();
-  }
 }
 </script>
 
 <template>
     <div v-if="estate" class="card mt-3">
         <div class="card-header text-center">
-            HomePage -      Prev - Next
+            HomePage - Prev - Next
         </div>
         <!-- images carousel -->
         <div v-if="estate.images" id="imagesCarousel" class="carousel slide my-2">
             <div class="carousel-inner">
-                <div v-for="image in estate.images" :key="image.id" class="carousel-item" :class="estate.images[0].id == image.id ? 'active' : ''">
+                <div v-for="image in estate.images" :key="image.id" class="carousel-item"
+                    :class="estate.images[0].id == image.id ? 'active' : ''">
                     <img :src="getImagePath(image)" class="estateImg d-block" alt="">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#imagesCarousel" data-bs-slide="prev">
-                <span :class="{'d-none' : estate.images}" class="btn btn-primary"><font-awesome-icon icon="fa-solid fa-backward" /></span>
+                <span :class="{ 'd-none': estate.images }" class="btn btn-primary"><font-awesome-icon
+                        icon="fa-solid fa-backward" /></span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#imagesCarousel" data-bs-slide="next">
-                <span :class="{'d-none' : estate.images}" class="btn btn-primary"><font-awesome-icon icon="fa-solid fa-forward" /></span>
+                <span :class="{ 'd-none': estate.images }" class="btn btn-primary"><font-awesome-icon
+                        icon="fa-solid fa-forward" /></span>
             </button>
         </div>
-        
+
         <div class="card-body">
             <!-- title -->
             <div class="estateTitle">
@@ -56,12 +59,12 @@ export default {
 
             <!-- address -->
             <div class="addressCard">
-                <BasicMap :estate="estate"/>
+                <BasicMap :estate="estate" />
             </div>
 
             <!-- description -->
             <div class="estateDescription">
-                <div><strong>Description</strong></div>
+                <div><strong>Descrizione</strong></div>
                 <p>{{ estate.description }}</p>
             </div>
 
@@ -97,7 +100,8 @@ export default {
 
 <style lang="scss" scoped>
 @use '@/assets/scss/partials/vars' as *;
-#imagesCarousel .btn.btn-primary{
+
+#imagesCarousel .btn.btn-primary {
     font-size: 1.5rem;
     font-weight: bold;
     color: $logo-color;
@@ -107,12 +111,11 @@ export default {
 }
 
 #imagesCarousel .carousel-control-next:hover .btn.btn-primary,
-#imagesCarousel .carousel-control-prev:hover .btn.btn-primary
-{
+#imagesCarousel .carousel-control-prev:hover .btn.btn-primary {
     opacity: 1;
 }
 
-.estateImg{
+.estateImg {
     min-width: 420px;
     max-width: 100%;
     height: 500px;
@@ -120,15 +123,13 @@ export default {
 }
 
 .iconService {
-  padding: 10px;
-  font-size: 1.6rem;
-  font-weight: 500;
-  border: 2px solid transparent;
+    padding: 10px;
+    font-size: 1.6rem;
+    font-weight: 500;
+    border: 2px solid transparent;
 }
 
-.card-body>*{
+.card-body>* {
     padding-bottom: 0.5rem;
 }
-
-
 </style>
