09a083249078d0844cbb0d93c2e5aa8ad1066b4f
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-10-03 12:38:49 +0200

    temporary change to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,33 +1,25 @@
 <script></script>
 <template>
-    <div class="container">
-        <footer>
-            <div>BoolBnB 2023 creato da: <span class="tribute">Claudiu -</span> <span class="tribute">Fabio -</span>
-                <span class="tribute"> Marco -</span><span class="tribute"> Nicola -</span><span class="tribute">
-                    Tommaso</span>
-            </div>
-
-        </footer>
-    </div>
+    <footer>
+        <div class="ms-2 opacity-25">
+            BoolBnB 2023 creato da:
+            <span class="tribute"> Claudiu -</span>
+            <span class="tribute"> Fabio -</span>
+            <span class="tribute"> Marco -</span>
+            <span class="tribute"> Nicola -</span>
+            <span class="tribute"> Tommaso</span>
+        </div>
+    </footer>
 </template>
 
 <Style scoped lang="scss">
 footer {
-    position: absolute;
-    bottom: 0px;
-    margin-left: 14px;
-}
-
-.h7 {
-    font-weight: 700;
+    position: fixed;
+    bottom: 0;
+    left: 0;
 }
 
 .tribute {
-
     font-weight: 700;
-}
-
-.tribute:hover {
-    color: #FFBD59;
 }
 </Style>
